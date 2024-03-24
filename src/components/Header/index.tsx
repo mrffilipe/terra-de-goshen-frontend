8516cdf171dfe5297b49f69185be85f779c88ecb
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-03-23 21:46:41 -0300

    Add adjustments Header componente

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
 
+import { Menu, Close } from '@mui/icons-material'
+
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState<boolean>()
+
+    const handleOpenMenu = (): void => {
+        setOpenMenu(prev => !prev)
+    }
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
                 <h1>Terra de Goshen</h1>
             </div>
             <nav className={styles.nav}>
-                <ul className={styles.menu}>
+                <button className={styles.open_menu} onClick={handleOpenMenu}>
+                    {
+                        openMenu ?
+                            <Close /> :
+                            <Menu />
+                    }
+                </button>
+                <ul className={`${styles.menu} ${openMenu ? '' : styles.hidden_menu}`}>
                     <li>
                         <a href="#">Início</a>
                     </li>
