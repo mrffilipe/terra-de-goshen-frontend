4dcd0ecb229d2ea84e628e65de84061a3a642ad7
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-04-06 16:08:01 -0300

    Add SignInPage

--- src/pages/AuthPage/SignInPage/index.tsx
+++ src/pages/AuthPage/SignInPage/index.tsx
@@ -1,9 +1,37 @@
 import styles from './styles.module.css'
 
+import { useState } from 'react'
+
 const SignInPage = () => {
+    const [email, setEmail] = useState<string>()
+    const [password, setPassword] = useState<string>()
+
     return (
         <article className={styles.sign_in_page}>
-
+            <div className={styles.form_container}>
+                <form className={styles.form}>
+                    <span>Login</span>
+                    <div>
+                        <label>
+                            E-mail
+                            <input
+                                type="email"
+                                placeholder='exemplo@exemplo.com'
+                                value={email}
+                                onChange={e => setEmail(e.target.value)} />
+                        </label>
+                        <label>
+                            Senha
+                            <input
+                                type="password"
+                                placeholder='••••••••'
+                                value={password}
+                                onChange={e => setPassword(e.target.value)} />
+                        </label>
+                    </div>
+                    <button type='submit'>Entrar</button>
+                </form>
+            </div>
         </article>
     )
 }
